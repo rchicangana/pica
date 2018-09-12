a64de1fc31bd1da14ea80e3ff775c37d1a29b8fd
Ricardo Chicangana <rchicangana@gmail.com>
2018-09-12 10:26:51 -0500

    Accesos a listas y manipulacion de las mismas

--- RedisServices/src/main/java/co/com/javeriana/redisservices/redis/RedisListResource.java
+++ RedisServices/src/main/java/co/com/javeriana/redisservices/redis/RedisListResource.java
@@ -6,11 +6,13 @@
 package co.com.javeriana.redisservices.redis;
 
 import co.com.javeriana.redisservices.redis.cdi.FromJedisPool;
-import java.util.List;
+import co.com.javeriana.redisservices.redis.dto.ListDTO;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import redis.clients.jedis.Jedis;
@@ -36,10 +38,30 @@ public class RedisListResource {
      * @return
      */
     @GET
-    @Path("lista/{list}/{ini}/{fin}")
-    public List<String> getList(@PathParam("list") String list, @PathParam("ini") Integer ini, @PathParam("fin") Integer fin){
+    @Path("/{list}/{ini}/{fin}")
+    public ListDTO getList(@PathParam("list") String list, @PathParam("ini") Integer ini, @PathParam("fin") Integer fin){
         LOG.log(Level.INFO, "Obteniendo informacion de la lista {0} del objeto {1} al {2}", new Object[]{list, ini, fin});
-        return jedisFromPool.lrange(list, ini, fin);
+        ListDTO salida = new ListDTO();
+        salida.setDatos(jedisFromPool.lrange(list, ini, fin));
+        return salida;
+    }
+    
+    @POST
+    @Path("/{list}")
+    public void putList(@PathParam("list") String lista, ListDTO entrada){
+        LOG.log(Level.INFO, "Registrando informacion de la lista {0} ", new Object[]{lista});
+        for (String dato : entrada.getDatos()) {
+            jedisFromPool.lpush(lista, dato);
+        }
+        
+    }
+    
+    @DELETE
+    @Path("/{list}")
+    public void removeList(@PathParam("list") String lista){
+        LOG.log(Level.INFO, "Removiendo informacion de la lista {0} ", new Object[]{lista});
+        jedisFromPool.del(lista);
+        
     }
     
     
---
+++ RedisServices/src/main/java/co/com/javeriana/redisservices/redis/dto/ListDTO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.javeriana.redisservices.redis.dto;
+
+import java.util.List;
+
+/**
+ *
+ * @author rchicangana
+ */
+public class ListDTO {
+    
+    private List<String> datos;
+
+    public List<String> getDatos() {
+        return datos;
+    }
+
+    public void setDatos(List<String> datos) {
+        this.datos = datos;
+    }
+
+    
+    
+    
+}
